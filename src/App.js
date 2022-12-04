8272093146b95f45632d430ea8b03ad74ea23512
Lale Mulati <100545087+LilyMurat@users.noreply.github.com>
2022-12-03 23:48:37 -0500

    added 4 js files about employee under pages folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Switch } from 'react-native';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import EmployeeList from "./pages/EmployeeList";
+import AddEmployee from "./pages/AddEmployee";
+import UpdateEmployee from "./pages/UpdateEmployee";
+import ViewEmployee from "./pages/ViewEmployee";
 
-function App() {
+import{ 
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom"
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={EmployeeList}/>
+          <Route exact path="/view/:id" component={ViewEmployee}/>
+          <Route exact path="/update/:id" component={UpdateEmployee}/>
+          <Route exact path="/add" component={AddEmployee}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
