4b9447bea962638b7efbab1226988446ed2194bf
Lale Mulati <100545087+LilyMurat@users.noreply.github.com>
2022-12-04 23:10:19 -0500

    debug

--- src/pages/AddEmployee.js
+++ src/pages/AddEmployee.js
@@ -1,72 +1,71 @@
-import React, { useState } from React;
-import axios from 'axios';
-import { useHistory, Link} from "react-router-dom";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory, Link } from "react-router-dom";
 
 const AddEmployee = () => {
-    let history = useHistory();
-    const [employee, setEmployee] = useState({
-        FirstName:"",
-        LastName:"",
-        emailId:"",
-    });
+  let history = useHistory();
+  const [employee, setEmployee] = useState({
+    FirstName: "",
+    LastName: "",
+    email: "",  
+  });
 
-const {FirstName, LastName, emailId} = employee;
-const onInputChange = e => {
-    setEmployee({...employee, [e.target.name]: e.target.value});
-};
+  const { FirstName, LastName, email} = employee;
+  const onInputChange = e => {
+    setEmployee({ ...employee, [e.target.name]: e.target.value });
+  };
 
-const onSubmit = async e =>{
+  const onSubmit = async e => {
     e.preventDefault();
-    await axios.post("/api/v1/employees",employee);
-    history.pushState("/");
-};
-return(
+    await axios.post("/api/v1/employees", employee);
+    history.push("/");
+  };
+  return (
     <div className="container py-4">
-        <div className="w-75 mx-auto shadow p-5">
-            <h2 className="text-center mb-4">Add Employee</h2>
-            <from onSubmit={e =>onSubmit(e)}>
-                <div className="form-group">
-                    <p>First Name: </p>
-                    <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        placeholder="Enter Your Name"
-                        name="FirstName"
-                        value={FirstName}
-                        onChange={e => onInputChange(e)}
-                    />
-                </div>
-
-                <div className="form-group">
-                    <p>Last Name: </p>
-                        <input
-                            type="text"
-                            className="form-control form-control-lg"
-                            placeholder="Enter Your Name"
-                            name="LastName"
-                            value={LastName}
-                            onChange={e => onInputChange(e)}
-                        />
-                </div>
-
-                <div className="form-group">
-                    <p>Email ID: </p>
-                        <input
-                            type="email"
-                            className="form-control form-control-lg"
-                            placeholder="Enter Your Name"
-                            name="emailId"
-                            value={emailId}
-                            onChange={e => onInputChange(e)}
-                        />
-                </div>
-                <button className="btn btn-primary btn-block" style={{marginTop: 10}}>Add Employee</button>
-                <Link className="btn btn-danger" style={{marginTop: 10, marginLeft: 10}} to="/">Cancel</Link>
-            </from>
-        </div>
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add Employee</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <p>First Name : </p>
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="FirstName"
+              value={FirstName}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          
+          <div className="form-group">
+            <p>Last Name : </p>
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your LastName"
+              name="LastName"
+              value={LastName}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          
+          <div className="form-group">
+            <p>Email : </p>
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block" style= {{marginTop : 10}}>Add Employee</button>
+          <Link className="btn btn-danger"style= {{marginTop : 10 , marginLeft : 10}} to="/">Cancel</Link>
+        </form>
+      </div>
     </div>
-)
+  );
 };
 
 export default AddEmployee;
-
--- src/pages/ViewEmployee.js
+++ src/pages/ViewEmployee.js
@@ -1,42 +1,41 @@
-import React, { useState, useEffect } from React; 
-import { Link, useParams } from "react-router-dom"; 
-import axios from 'axios'; 
- 
-const ViewEmployee = () => { 
-  const [employee, setEmployee] = useState({ 
-    firstname: "", 
-    lastname: "", 
-    emailId: "", 
- 
-  }); 
-  const { id } = useParams(); 
-  useEffect(() => { 
-    loadEmployee(); 
-  }, []); 
-  const loadEmployee = async () => { 
-    const res = await axios.get(`/api/v1/employees/${id}`); 
-    setEmployee({ 
-      firstname: res.data.FirstName, 
-      lastname: res.data.LastName, 
-      emailId: res.data.emailId, 
-      id : res.data._id 
-    }); 
-  }; 
-  return ( 
-    <div className="App container py-4"> 
-      <h1 className="display-4">{employee.firstname}'s profile</h1> 
-      <hr /> 
-      <ul className="list-group w-40"> 
-        <li className="list-group-item">FirstName: {employee.firstname}</li> 
-        <li className="list-group-item">user name: {employee.lastname}</li> 
-        <li className="list-group-item">email: {employee.email}</li> 
-      </ul><br/> 
-      <Link className="btn btn-primary" to="/"> 
-        return 
-      </Link> 
-    </div> 
-  ); 
-}; 
- 
-export default ViewEmployee;
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
+const ViewEmployee = () => {
+  const [employee, setEmployee] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+
+  });
+  const { id } = useParams();
+  useEffect(() => {
+    loadEmployee();
+  }, []);
+  const loadEmployee = async () => {
+    const res = await axios.get(`/api/v1/employees/${id}`);
+    setEmployee({
+      firstname: res.data.FirstName,
+      lastname: res.data.LastName,
+      email : res.data.email,
+      id : res.data._id
+    });
+  };
+  return (
+    <div className="App container py-4">
+      <h1 className="display-4">{employee.firstname}'s profile</h1>
+      <hr />
+      <ul className="list-group w-40">
+        <li className="list-group-item">FirstName: {employee.firstname}</li>
+        <li className="list-group-item">user name: {employee.lastname}</li>
+        <li className="list-group-item">email: {employee.email}</li>
+      </ul><br/>
+      <Link className="btn btn-primary" to="/">
+        return
+      </Link>
+    </div>
+  );
+};
+
+export default ViewEmployee;
